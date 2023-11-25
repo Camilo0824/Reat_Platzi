10f8d9ea85143afe5b0f7bb246232b364dbc867e
Camilo Barbosa <cbarbosa@identidadtech.com>
2023-11-24 22:05:04 -0500

    Create components for Todo Machine

---
+++ src/TodoCounter.js
@@ -0,0 +1,9 @@
+function TodoCounter() {
+    return (
+        <h1>
+            You have completed 4 tasks out of 10.
+        </h1>
+    );
+}
+
+export { TodoCounter };
---
+++ src/TodoItem.js
@@ -0,0 +1,11 @@
+function TodoItem() {
+    return (
+        <li>
+            <span>V</span>
+            <p>Grocery shopping</p>
+            <span>X</span>
+        </li>
+    );
+}
+
+export { TodoItem };
