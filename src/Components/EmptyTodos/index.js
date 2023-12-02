aa7a926f96a5cb6a4cef387507dc7d07bb5fe300
Camilo Barbosa <cbarbosa@identidadtech.com>
2023-12-01 19:13:20 -0500

    Add loader to app

---
+++ src/Components/EmptyTodos/index.js
@@ -0,0 +1,10 @@
+import './EmptyTodos.css'
+import React from 'react';
+
+function EmptyTodos() {
+  return (
+    <p>Create your first TODO!</p>
+  );
+}
+
+export { EmptyTodos };
---
+++ src/Components/TodosError/index.js
@@ -0,0 +1,10 @@
+import './TodosError.css'
+import React from 'react';
+
+function TodosError() {
+  return (
+    <p>There was an mistake :c </p>
+  );
+}
+
+export { TodosError };
